ef0b8171957e694ed6bdc0fbb6a7ed54aa6e057d
camilo henriquez <camilo.henriquez@simplit-solutions.com>
2022-01-26 23:18:10 -0300

    in memory large array

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
-from large_array import large_array
+from generate_large_array import random_array
 from sorting_algorithms.bubble_sort import bubble_sort
 from sorting_algorithms.max_heap import max_heap_sort as tree_mh_sort
 from sorting_algorithms.max_heap_arr import max_heap_sort as array_mh_sort
+
+large_array = random_array(array_length=7000, biggest_number=1000000000000000)
+
 
 # heap sort using max heap with binary tree
 tree_mh_sort_result = tree_mh_sort(large_array[:])
